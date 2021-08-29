a4e64c61ab55cb8687d12c570af78cf45309eff2
Madan AR <madanar@106AMBBLR-Nithin-A-Y.local>
2021-08-29 17:08:24 +0530

    added a method to input the user choice

--- src/com/tictactoe/TicTacToeGame.java
+++ src/com/tictactoe/TicTacToeGame.java
@@ -1,18 +1,49 @@
 package com.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 
+	private char[] board = new char[10];
+	
 	public static void main(String[] args) {
-		char[] board = new char[10];
-		board[1] = ' ';
-		board[2] = ' ';
-		board[3] = ' ';
-		board[4] = ' ';
-		board[5] = ' ';
-		board[6] = ' ';
-		board[7] = ' ';
-		board[8] = ' ';
-		board[9] = ' ';
+	
+		System.out.println("welcome to the game");
+		TicTacToeGame tic = new TicTacToeGame();
+		tic.createBoard();
+		tic.input();
+		
 	
 	}
+	
+	public void createBoard() {
+		for(int i=1; i<board.length; i++)
+			board[i] = ' ';
+	}
+	
+	public void input() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the choice X or O");
+		char userChoice = sc.next().charAt(0);
+		char computerChoice ;
+		if( userChoice == 'X')
+		{
+			computerChoice = 'O';
+		}
+		else if( userChoice == 'O') {
+			computerChoice = 'X';
+		}
+		else {
+			userChoice = 'X';
+			computerChoice = 'O';
+			System.out.println("default choice is X");
+		}
+		System.out.println("Your choice is " + userChoice);
+		System.out.println("Computer choice is " + computerChoice);
+		
+	   
+	}
+	
+	
 }
